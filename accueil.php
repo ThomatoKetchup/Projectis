2ff413905a921b9ad5990b17f01e3a5f3c245f15
ThomatoKetchup <33425389+ThomatoKetchup@users.noreply.github.com>
2017-11-20 10:53:07 +0100

    Ajout de la page d'accueil

---
+++ accueil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+
+        <meta charset="utf-8" />
+
+        <title>Titre de notre site</title>
+
+    </head>
+
+
+    <body>
+    <h1>Réseau social pour le voyage</h1>
+
+
+    </body>
+
+</html>
